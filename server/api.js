da3658eb883874cafe6ad3d01f532905239a0f7d
Evan Genest <genest.1@osu.edu>
2018-01-31 20:18:50 -0600

    Add header comments

--- server/api.js
+++ server/api.js
@@ -1,3 +1,12 @@
+/*************************************************/
+/*        January 31, 2018 by Evan Genest        */
+/* "Boss Machine" for "Build Your Own APIs"      */
+/*          at Codecademy                        */
+/*                       */
+/**/
+/*****************************************************/
+
+
 const express = require('express');
 const apiRouter = express.Router();
 
--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,3 +1,12 @@
+/*************************************************/
+/*        January 31, 2018 by Evan Genest        */
+/* "Boss Machine" for "Build Your Own APIs"      */
+/*          at Codecademy                        */
+/*                       */
+/**/
+/*****************************************************/
+
+
 const checkMillionDollarIdea = () => {};
 
 // Leave this exports assignment so that the function can be used elsewhere
