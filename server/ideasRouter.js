88004208ee04e2c27260505e4157ce6f1b866fdb
Evan Genest <genest.1@osu.edu>
2018-02-09 19:39:14 -0600

    Make the ideasRouter respond in the browser for a blank request to 'ideas'

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -4,16 +4,18 @@ console.log(`Reporting for duty: "ideasRouter".`);
 
 let ideas = ["Pigs with wings.", "Solid gold toilets", "Newsradio with POTUS-45-mute function"];
 
+
 ideasRouter.get("/", (req, res, next)=>{
-	console.log(`How about these: ${ideas} ?`);
+	res.send(`How about these:  {ideas} ?`);
 	next();
 }  );
 
+/*
 ideasRouter.get("/:id", (req, res, next)=>{
 	let n = req.params.id;
-	console.log(`My fave idea is ${ideas[n]}`);
+	res.send(`My fave idea is {ideas[n]}`);
 	return;
 } );
-
+*/
 
 module.exports = (ideasRouter);
