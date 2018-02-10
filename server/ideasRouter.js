3b1833a2fbe462a97ef1fd05be9b53fcbff9429a
Evan Genest <genest.1@osu.edu>
2018-02-09 19:18:59 -0600

    No js errors but routes not at all working  yet.

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,17 +1,19 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const ideasRouter = express.Router();
 console.log(`Reporting for duty: "ideasRouter".`);
+
+let ideas = ["Pigs with wings.", "Solid gold toilets", "Newsradio with POTUS-45-mute function"];
+
+ideasRouter.get("/", (req, res, next)=>{
+	console.log(`How about these: ${ideas} ?`);
+	next();
+}  );
+
+ideasRouter.get("/:id", (req, res, next)=>{
+	let n = req.params.id;
+	console.log(`My fave idea is ${ideas[n]}`);
+	return;
+} );
+
+
 module.exports = (ideasRouter);
