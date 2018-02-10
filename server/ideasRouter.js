a9bc0b8de4b4ac87fd30c35ab648aab5ae53be73
Evan Genest <genest.1@osu.edu>
2018-02-09 20:02:57 -0600

    Make ideas route work when hard coded for 'localhost:3000/ideas/0' but not for parsing id.

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const ideasRouter = express.Router();
 console.log(`Reporting for duty: "ideasRouter".`);
+// the following utils are totally from a codec lesson
+const { getElementById, getIndexById, updateElement, seedElements, createElement } = require('./utilsCA.js');
+
+
 
 let ideas = ["Pigs with wings.", "Solid gold toilets", "Newsradio with POTUS-45-mute function"];
 
@@ -10,12 +14,22 @@ ideasRouter.get("/", (req, res, next)=>{
 	next();
 }  );
 
-/*
+// This route works
+ideasRouter.get("/0", (req, res, next)=>{
+	//let n = parseInt(req.params.id); // add 10 ?
+	let n = 0;
+	console.log(`Parsed this --${n}--` );
+	res.send(`My fave idea is ${ideas[n]}`);
+	return;
+} );
+
+// This route does not
 ideasRouter.get("/:id", (req, res, next)=>{
-	let n = req.params.id;
+	let n = parseInt(req.params.id); // add 10 ?
+	console.log(`Parsed this --${n}--` );
 	res.send(`My fave idea is {ideas[n]}`);
 	return;
 } );
-*/
+
 
 module.exports = (ideasRouter);
