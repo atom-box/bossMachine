2363e78f1a0721b592b1eb08bd58444e29101b8e
Evan Genest <genest.1@osu.edu>
2018-02-25 17:14:04 -0600

    Make IDEAS router (3rd rtr of 3 rtrs) by cloning other two.

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,43 +1,88 @@
-/* NOT     */
-/* ONLINE  */
-/* YET     */
+console.log(``);
+const {getAllFromDatabase, getFromDatabaseById ,addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require("./db.js");
+express = require("express");
+ideasRouter = express();
 
 
-const express = require('express');
-const ideasRouter = express.Router();
-console.log(`Reporting for duty: "ideasRouter".`);
-// COMMENT OUT THESE
-// const { getElementById, getIndexById, updateElement, seedElements, createElement } = require('./utilsCA.js');
+/*   MY ROUTE works fine, better than the solution */
+ideasRouter.get(`/`, (req, res, next) => {
+	let itWorked = undefined;
+	itWorked = getAllFromDatabase("ideas");
+	if (!itWorked){
+		res.status(400);
+		const sorrow = new Error("GET-ALL IDEE not working.");
+		next(sorrow);
+	} else {
+		res.send(itWorked);
+		next(); 
+	}
+} );
 
-// USE THESE INSTEAD
-const {getAllFromDatabase, getFromDatabaseById ,addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId, deleteAllFromDatabase} = require("./db.js");
+// if not working CHECK TYPE, IN 3rd line
+ideasRouter.get("/:id", (req, res, next)=>{
+	const id = req.params.id;
+	const itFound = getFromDatabaseById("ideas", id);
+	if (!itFound) {
+		const sorrow = new Error("The server ID-parser didn't get anything");
+		next(sorrow);
+	} else {
+		res.send(itFound);
+		next();
+	}
+} );
 
+ideasRouter.post("/", (req, res, next )=>{
+	const victory = null;
+	const sorrow = null;
+	victory = addToDatabase("ideas", req.body);
+	if (victory){res.status(201).send(req.body);
+	} else {
+		res.status(400);
+		sorrow = new Error("Did not post a minion; database interaction problem.");
+		next(sorrow);
+	}
+} );
 
-
-let ideas = ["Pigs with wings.", "Solid gold toilets", "Newsradio with POTUS-45-mute function"];
-
-
-ideasRouter.get("/", (req, res, next)=>{
-	res.send(`How about these:  {ideas} ?`);
-	next();
+ideasRouter.put("/:id", (req, res, next)=>{
+	const victory = null;
+	victory = updateInstanceInDatabase("ideas", req.body);
+	if (victory){
+		res.send(victory); 
+	} else {
+		res.status(400);
+		const sorrow = new Error("Failed to update idea -- i.d. might be missing.");
+		next(sorrow);
+	}
 }  );
 
-// This route works
-ideasRouter.get("/0", (req, res, next)=>{
-	//let n = parseInt(req.params.id); // add 10 ?
-	let n = 0;
-	console.log(`Parsed this --${n}--` );
-	res.send(`My fave idea is ${ideas[n]}`);
-	return;
-} );
+ideasRouter.delete("/:id", (req, res, next)=>{
+	const victory = null;
+	victory = deleteFromDatabasebyId("ideas", req.params.id);
+	if (victory){res.send(victory) } else {
+		sorrow = new Error("Deleting went less than ideally: failed to find idea by ID to delete.");
+		next(sorrow);
+	};
+}  );
 
-// This works; now
-ideasRouter.get("/:id", (req, res, next)=>{
-	let n = parseInt(req.params.id, 10); // add 10 ?
-	console.log(`Parsed this --${n}--` );
-	res.send(`My fave idea is ${ideas[n]}`);
-	return;
-} );
+minionsRouter.use( (err, req, res, next)=>{
+	res.status(500);
+	console.error(err);
+	res.send("Ev's error, in the ideas level of routes.");
+}   );
+
+
+
+
+/******************************************/
+/*    January 31, 2018 by Evan Genest     */
+/* "Boss Machine" for "Build Your Own APIs"*/
+/*          at Codecademy              */
+/*        https://github.com/atom-box  */
+/**/
+/***************************************/
+
+console.log("ideas router present");
+
 
 
 module.exports = ideasRouter;
