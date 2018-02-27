8489ad17bd737e4b60b59079583888a0e0e62267
Evan Genest <genest.1@osu.edu>
2018-02-27 12:46:27 -0600

    Connect the Red Interface.  Moved export lower OR uncommented two of the routes in the API.JS

--- server.js
+++ server.js
@@ -4,63 +4,42 @@
 /*          at Codecademy                  */
 /*                                         */
 /***************************************/
+console.log(`Hubba...`);
 
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors);
 app.use(morgan('tiny'));
+
 // TODO errorhandler is here in 4.2.3
 console.log(`Line 15 in server.js.`);
-// TODO -- wot's dis?
-module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
 *  the frontend application to interact as planned with the api server
 */
 
-//console.log(`HERE IS PROCESS DOT ENV keys:${Object.keys(process.env)} evanARTHUR`);
-//let envKeys =[];
-//let x = "";
-//envKeys = Object.keys(process.env);
-//for (x in envKeys){
-//	console.log(`[${x}] is: ${process.env[x]}.`);
-//}
+
 const PORT = process.env.PORT || 4001;
-app.use(bodyParser);
 // Add middleware for handling CORS requests from index.html
+app.use(cors());
 
 
 // Add middware for parsing request bodies here:
-// TEMP EVAN
-const {getAllFromDatabase, getFromDatabaseById ,addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId, deleteAllFromDatabase} = require("./server/db.js");
-app.get(`/api/ideas/`, (req, res, next) => {
-	console.log(`This console log worked, inside the IDEAS ROUTE`);
-	let itWorked = undefined;
-	itWorked = getAllFromDatabase("ideas");
-	res.send(itWorked);
-} );
 
-/*	if (!itWorked){
-		res.status(400).send("Found less than ALL minions; found none.  GET failed.");
-		return;
-	} else {
-		res.send(itWorked);
-		next(); 
-	}
-} );
-
-app.get("api/minions/:id", (req, res, next)=>{
-	res.send(`They want ${req.params.id} or maybe ${id}.`);
-	next();
-} );
-*/
-
+app.use(bodyParser.json());
 // Mount your existing apiRouter below at the '/api' path.
-// UNCOMMENT!   const apiRouter = require('./server/api.js');
-// UNCOMMENT! app.use("/api", apiRouter);
+
+const apiRouter = require('./server/api.js');
+app.get("/", (req, res)=>{
+	res.send("This is the Boss Machine, my friend.")
+} );
+app.use("/api", apiRouter);
+
+// TODO -- originally up on top, like LINE EIGHTEEN ??
+module.exports = app;
+
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
--- server/api.js
+++ server/api.js
@@ -1,17 +1,18 @@
 console.log(`...hubba...`);
 
 const express = require('express');
-//const meetingsRouter =require("./meetingsRouter.js");
-//const ideasRouter = require("./ideasRouter.js");
+const meetingsRouter =require("./meetingsRouter.js");
+const ideasRouter = require("./ideasRouter.js");
 const minionsRouter = require("./minionsRouter.js");
-//const meetingsRouter = require("./meetingsRouter.js");
 const apiRouter = express.Router();
 
 console.log(`Line 23 in API.js.`);
-//apiRouter.use("/meetings", meetingsRouter);
+apiRouter.use("/meetings", meetingsRouter);
 apiRouter.use("/minions", minionsRouter);
-//apiRouter.use("/ideas", ideasRouter);
-//apiRouter.use("/meetings", meetingsRouter);
+apiRouter.use("/ideas", ideasRouter);
+apiRouter.get('/', (req, res, next)=>{
+	res.send("Come in. Come in to the API.")
+}  );
 
 console.log(`...hubba!`);
 module.exports = apiRouter;
