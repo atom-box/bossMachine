ecdeacb23ccb8b332ecf8c46b61484bba8689057
Evan Genest <genest.1@osu.edu>
2018-03-01 19:06:59 -0600

    Write function to return -3, -2, -1, 0, 1 as flags for checker of mill doll idea.

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -11,7 +11,33 @@
 // in 
 // http://expressjs.com/th/api.html
 
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (weeks, rate) => {
+	const net = weeks * rate;
+	if (!net){
+		const sorrow = new Error("Idea rate and/or weeks must both be a 'number'.");
+		req.status(409);
+		return -1; //  non numeric input!
+	}
+	if (weeks <= 0 || rate <= 0){
+		const sorrow = new Error("Idea rate and/or weeks must both be positive #s.");
+		req.status(409);
+		return -2; // 	 negative input # !	
+	} 
+	if (net < 1000000){
+		req.status(209);
+		return 0; //  not a million $ idea!		
+	} 
+	if (net >= 1000000){
+		return 1; // 		
+	} 
+	console.log("Line 33 bug in CMDI");
+	return -3; // should never reach here!   
+}; // error flags are, in order checked
+// -1 a non number in input
+// -2 a negative number in input
+// 0 not a million dollar idea
+// 1  SUCCESS!   MDI !
+// -3 oops, ran past everything; code bug!
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
