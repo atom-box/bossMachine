34e0732973381a39eac549739ff1c084a1eb3dad
Evan Genest <genest.1@osu.edu>
2018-03-03 08:46:10 -0600

    #Send it a test error from IDEAS ROUTER.

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,15 +1,22 @@
 console.log(`& ideas router &`);
 const {getAllFromDatabase, getFromDatabaseById ,addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require("./db.js");
-const checkMillionDollarIdea = require("./server/checkMillionDollarIdea.js" );
+const checkMillionDollarIdea = require("./checkMillionDollarIdea.js" );
 const express = require("express");
 const ideasRouter = express({mergeParams: true});
 
 const bodyParser = require('body-parser');
 ideasRouter.use(bodyParser.json());
 
+//test
+/*  ideasRouter( (req, res, next)=>{
+	const x = new Error ("Dummy load.  Remove ASAP.  ideasRouter Line 11.");
+	next(x);
+});  */
 
 /*   MY ROUTE works fine, better than the solution */
 ideasRouter.get(`/`, (req, res, next) => {
+	const x = new Error ("Dummy load.  Remove ASAP.  ideasRouter Line 11."); //***
+	next(x); // ****
 	let itWorked = undefined;
 	itWorked = getAllFromDatabase("ideas");
 	if (!itWorked){
@@ -36,19 +43,32 @@ ideasRouter.get("/:id", (req, res, next)=>{
 } );
 
 ideasRouter.post("/", (req, res, next )=>{
-	const victory = null;
+	let victory = null;
 	const sorrow = null;
+	switch (checkMillionDollarIdea(idea)){
+		case -3: 
+			sorrow = new Error("Ideas router says Bug caused CMDI to reach end inconclusively.");
+			next(err);
+		case -2: 
+			sorrow = new Error("Ideas router says negative number was input");
+			next(err);
+		case -1: 
+			sorrow = new Error("Ideas router says non-number was passed to CMDI");
+			next(err);
+		case -0: 
+			console.log(`Somehow, not sure how, need to display NOT A MILL DOLL IDEE.  To do . . .`);
+	}
 	victory = addToDatabase("ideas", req.body);
 	if (victory){res.status(201).send(req.body);
 	} else {
 		res.status(400);
-		sorrow = new Error("Did not post a minion; database interaction problem.");
+		sorrow = new Error("Did not post an idea; database interaction problem.");
 		next(sorrow);
 	}
 } );
 
 ideasRouter.put("/:id", (req, res, next)=>{
-	const victory = null;
+	let victory = null;
 	victory = updateInstanceInDatabase("ideas", req.body);
 	if (victory){
 		res.send(victory); 
@@ -60,7 +80,7 @@ ideasRouter.put("/:id", (req, res, next)=>{
 }  );
 
 ideasRouter.delete("/:id", (req, res, next)=>{
-	const victory = null;
+	let victory = null;
 	victory = deleteFromDatabasebyId("ideas", req.params.id);
 	if (victory){res.send(victory) } else {
 		sorrow = new Error("Deleting went less than ideally: failed to find idea by ID to delete.");
@@ -68,11 +88,11 @@ ideasRouter.delete("/:id", (req, res, next)=>{
 	};
 }  );
 
-ideasRouter.use( (err, req, res, next)=>{
+/*    ideasRouter.use( (err, req, res, next)=>{
 	res.status(500);
 	console.error(err);
 	res.send("Ev's error, in the ideas level of routes.");
-}   );
+}   );  */
 
 
 
